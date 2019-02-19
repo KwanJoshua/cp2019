575cd1999187c4887e21fadea0776d68d3c77531
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-02-19 21:48:06 +0800

    Update q2_calc_cylinder_volume.py

--- q2_calc_cylinder_volume.py
+++ q2_calc_cylinder_volume.py
@@ -1,1 +1,12 @@
+# Enter Input
+radius = float(input('Radius (m): '))
+length = float(input('Length (m): '))
 
+# Calculation
+from math import pi
+area = 2*radius**2*pi + 2*pi*radius*length
+volume = radius**2*length*pi
+
+# Output
+print('Area (m^2): ', area)
+print('Volume (m^3): ', volume)

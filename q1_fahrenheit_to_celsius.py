33c8c7f5d837826e0070b756f0df6de688229b9a
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-02-19 21:33:31 +0800

    Create q1_fahrenheit_to_celsius.py

---
+++ q1_fahrenheit_to_celsius.py
@@ -0,0 +1,8 @@
+# Enter input
+Fahrenheit = float(input('Temperature (Fahrenheit): '))
+
+# Conversion to celcius
+Celcius = (5/9)*(Fahrenheit - 32)
+
+# Output
+print('Temperature (Celcius) : ',Celcius)

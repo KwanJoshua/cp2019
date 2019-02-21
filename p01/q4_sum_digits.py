115df021963835fd508b59ed861bfadb29f0c8ec
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-02-21 22:59:00 +0800

    Update q4_sum_digits.py

--- p01/q4_sum_digits.py
+++ p01/q4_sum_digits.py
@@ -1,1 +1,12 @@
+# Enter Input
+number= int(input('Enter Number: '))
 
+# Calculation
+digits = 0
+while (number > 10):
+    digits+=number%10
+    number = number//10
+digits+=number
+
+# Output
+print(digits)

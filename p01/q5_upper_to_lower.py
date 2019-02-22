28a0d73ab4127b380f1f6deae0ed4ca73c668cfa
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-02-22 15:40:43 +0800

    Update q5_upper_to_lower.py

--- p01/q5_upper_to_lower.py
+++ p01/q5_upper_to_lower.py
@@ -1,1 +1,14 @@
+# Enter Input
+word = str(input('Enter Word: '))
+w = []
+s = []
 
+# Changing into lowercase letter
+for i in word:
+    w.append(ord(i)+32)
+
+for i in w:
+    s.append(chr(i))
+
+# Output
+print(''.join(s))

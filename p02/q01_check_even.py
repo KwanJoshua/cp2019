eb5d1068712042a7120009017316965c4aa672a8
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-02-22 16:19:28 +0800

    Update q01_check_even.py

--- p02/q01_check_even.py
+++ p02/q01_check_even.py
@@ -1,1 +1,8 @@
+# Enter Input
+number = int(input('enter number: '))
 
+#Determining Even Or Odd And Giving Output
+if number%2 == 0:
+    print(number, 'is even')
+else:
+    print(number, 'is odd')

bc080baca2f6ad3e3e66d9f3fcbc915f53d41c9f
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-02-25 19:59:46 +0800

    Update q04_determine_leap_year.py

--- p02/q04_determine_leap_year.py
+++ p02/q04_determine_leap_year.py
@@ -2,7 +2,7 @@
 year = int(input('Enter year: '))
 
 # Main
-if year%400 == 0 or year%100 != 0 and year%4 == 0:
+if year%400 == 0 or (year%100 != 0 and year%4) == 0:
     print('Leap')
 else:
     print('Non-Leap')

331555a019e67320044e0eeb60437d86538b145c
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-02-25 19:40:21 +0800

    Update q02_triangle.py

--- p02/q02_triangle.py
+++ p02/q02_triangle.py
@@ -1,1 +1,10 @@
+# Enter Input
+s1 = int(input('Enter side 1: '))
+s2 = int(input('Enter side 2: '))
+s3 = int(input('Enter side 3: '))
 
+# Main
+if s1+s2 > s3 and s2+s3 > s1 and s1+s3 >s2:
+    print(s1 + s2 + s3)
+else:
+    print('Invalid triangle!')

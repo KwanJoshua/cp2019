3bad5ae69ca1be4c2cb4f6c2d5d7759555322508
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-02-25 20:47:44 +0800

    Update q06_kilograms_to_pounds.py

--- p02/q06_kilograms_to_pounds.py
+++ p02/q06_kilograms_to_pounds.py
@@ -1,1 +1,13 @@
+# Main
+kg = 1
 
+print('Kilograms', 'Pounds')
+for i in range(9):
+    pounds = kg*2.2
+    pounds = '{0:.1f}'.format(pounds)
+    print(kg, f'{pounds:^22}')
+    kg += 1
+for i in range (10,11):
+    pounds = kg*2.2
+    pounds = '{0:.1f}'.format(pounds)
+    print(kg, f'{pounds:^20}')

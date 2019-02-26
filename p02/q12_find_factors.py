fb1c5dbb853df8f265bd6382b7f3098cf9c6767a
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-02-26 21:34:37 +0800

    Update q12_find_factors.py

--- p02/q12_find_factors.py
+++ p02/q12_find_factors.py
@@ -1,1 +1,15 @@
+# Enter Input
+n = int(input('Enter number: '))
 
+# Main
+i = 1
+n1 = n
+factor = []
+while (0 < i <= n):
+    if n1 % i == 0:
+        factor.append(i)
+        i += 1
+    else:
+        i += 1
+
+print(','.join(map(str, factor)))

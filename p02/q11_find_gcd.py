a1ae9134f571ecc6aef7c6d7cec66a3257a6b564
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-02-26 21:27:39 +0800

    Update q11_find_gcd.py

--- p02/q11_find_gcd.py
+++ p02/q11_find_gcd.py
@@ -1,1 +1,21 @@
+# Enter Input
+n1 = int(input('Enter first number: '))
+n2 = int(input('Enter second number '))
 
+# Main
+if n1 < n2:
+    i = n2
+    while (0 < i <= n2):
+        if n2 % i == 0 and n1 % i == 0:
+            print('Greatest common divisor is', i)
+            break
+        else:
+            i -= 1
+else:
+    i = n1
+    while (0 < i <= n1):
+        if n1 % i == 0 and n2 % i == 0:
+            print('Greatest common divisor is', i)
+            break
+        else:
+            i -= 1

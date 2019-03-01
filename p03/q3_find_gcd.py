2ecf4808a28cf7a186d573fced7d9e2a13d20be6
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-03-01 23:44:27 +0800

    Update q3_find_gcd.py

--- p03/q3_find_gcd.py
+++ p03/q3_find_gcd.py
@@ -1,1 +1,34 @@
+# Enter Input
+num_1 = int(input('Enter first number: '))
+num_2 = int(input('Enter second number: '))
 
+# Main
+def gcd(m, n):
+    cd = []
+    a = 1
+    b = 1
+    i = 1
+
+    while i <= (m):
+        if m % i == 0:
+            cd.append(i)
+            i += 1
+        else:
+            i += 1
+    
+    while a <= (n):
+        if n % a == 0:
+            cd.append(a)
+            a += 1
+        else:
+            a += 1
+
+    cd.sort()
+    while b <= len(cd):
+        if cd[-b] == cd[-(b+1)]:
+            print('GCD =',cd[-b])
+            break
+        else:
+            b+=1
+
+gcd(num_1, num_2)

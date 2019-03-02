0fced89ae981f1d9e9fb60b1522838ab40e22823
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-03-03 01:12:52 +0800

    Update q6_display_matrix.py

--- p03/q6_display_matrix.py
+++ p03/q6_display_matrix.py
@@ -1,1 +1,19 @@
+# Enter Input
+num = int(input('Enter number: '))
 
+# Function
+def print_matrix(n):
+    import random
+    a = 0
+    i = 0
+    matrix = []
+    while i < (n**2):
+        matrix.append(random.randint(0, 1))
+        i += 1
+    while a < (n**2):
+        for b in matrix[a:a+n]:
+            print(b, end = ' ')
+        a += n
+        print('')
+
+print_matrix(num)

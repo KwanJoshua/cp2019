dccfb24aed2a2a00bd3700c0880371e049f0f125
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-03-03 01:24:50 +0800

    Update q7_convert_milliseconds.py

--- p03/q7_convert_milliseconds.py
+++ p03/q7_convert_milliseconds.py
@@ -1,1 +1,17 @@
+# Enter Input
+num = int(input('Enter number: '))
 
+# Function
+def convert_ms(n):
+    time = []
+    h = n//3600000
+    m = n//60000
+    s = n//1000
+    time.append(h)
+    time.append(':')
+    time.append(m - h*60)
+    time.append(':')
+    time.append(s - m*60)
+    for i in time:
+        print(i, end = '')
+convert_ms(num)

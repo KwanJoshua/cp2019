bbfca6fa0c7876412d759064c3c8c61dc2552311
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-03-06 08:30:46 +0800

    Update q1_sum_series1.py

--- p04/q1_sum_series1.py
+++ p04/q1_sum_series1.py
@@ -1,1 +1,14 @@
+# Enter Input
+num = int(input('Enter number: '))
 
+# Function
+def sum_series1(i):
+    n = 1
+    total = 0
+    while n <= i:
+        total += (1/n)
+        n += 1
+    print(total)
+
+# Main
+sum_series1(num)

6c5d1e0e52f04e3ef7c1b1d00e121de23ef7b4ad
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-03-06 08:33:44 +0800

    Update q2_sum_series2.py

--- p04/q2_sum_series2.py
+++ p04/q2_sum_series2.py
@@ -1,1 +1,14 @@
+# Enter Input
+num = int(input('Enter number: '))
 
+# Function
+def sum_series2(i):
+    n = 1
+    total = 0
+    while n <= i:
+        total += (n/(2*n + 1))
+        n += 1
+    print(total)
+
+# Main
+sum_series2(num)

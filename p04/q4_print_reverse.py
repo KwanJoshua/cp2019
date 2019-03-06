4d620f5e5da3d4631570ce83bff61a4c618e2147
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-03-06 19:46:04 +0800

    Update q4_print_reverse.py

--- p04/q4_print_reverse.py
+++ p04/q4_print_reverse.py
@@ -1,1 +1,13 @@
+# Enter Input
+num = int(input('Enter number: '))
 
+# Funtion
+def reverse_int(n):
+    if n > 0:
+        print(n % 10, end='')
+        reverse_int(n // 10)
+    elif n == 0:
+        print('')
+            
+# Main
+reverse_int(num)

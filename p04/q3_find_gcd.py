d6d26bdfc56b2f2075a4e1a10867a397a18662c0
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-03-06 14:37:16 +0800

    Update q3_find_gcd.py

--- p04/q3_find_gcd.py
+++ p04/q3_find_gcd.py
@@ -1,1 +1,13 @@
+# Enter Input
+num_1 = int(input('Enter number: '))
+num_2 = int(input('Enter number: '))
 
+# Funtion
+def gcd(m, n):
+    if m % n == 0:
+        print(n)
+    else:
+        gcd(n, m % n)
+
+# Main
+gcd(num_1, num_2)

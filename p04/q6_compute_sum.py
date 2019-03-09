01730ee09e4120e672e9f22b207351c668ec623a
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-03-09 21:05:06 +0800

    Update q6_compute_sum.py

--- p04/q6_compute_sum.py
+++ p04/q6_compute_sum.py
@@ -1,1 +1,12 @@
+# Enter Input
+num = int(input('Enter number: '))
 
+# Function
+def sum_digits(n):
+    if n > 0:
+        return n%10 + sum_digits(n//10)
+    else:
+        return 0
+
+# Main
+print(sum_digits(num))

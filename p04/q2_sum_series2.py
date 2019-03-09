95127f5f8efc80cf1a3df13faea1d30ff103c3a3
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-03-09 21:31:36 +0800

    Update q2_sum_series2.py

--- p04/q2_sum_series2.py
+++ p04/q2_sum_series2.py
@@ -3,12 +3,10 @@ num = int(input('Enter number: '))
 
 # Function
 def sum_series2(i):
-    n = 1
-    total = 0
-    while n <= i:
-        total += (n/(2*n + 1))
-        n += 1
-    print(total)
+    if i > 0:
+        return (i / (2*i + 1)) + sum_series2(i-1)
+    else:
+        return 0
 
 # Main
-sum_series2(num)
+print(sum_series2(num))

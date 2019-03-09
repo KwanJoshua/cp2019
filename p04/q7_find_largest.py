392607cc61ef138dff78f16023b5525fbba11ab4
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-03-09 21:57:47 +0800

    Update q7_find_largest.py

--- p04/q7_find_largest.py
+++ p04/q7_find_largest.py
@@ -2,12 +2,16 @@
 length = int(input('Enter length of list: '))
 alist = []
 for i in range (length):
-    alist.append(input('Enter number: '))
+    alist.append(int(input('Enter number: ')))
 
 # Funtion
 def find_largest(n):
-    n.sort()
-    print(n[-1])
-
+    if len(n) > 0:
+        if n[0] > find_largest(n[1:]):
+            return n[0]
+        else:
+            return find_largest(n[1:])
+    else:
+        return 0
 # Main
-find_largest(alist)
+print(find_largest(alist))

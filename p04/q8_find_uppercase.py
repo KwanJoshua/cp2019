3409b2c29bcc73ed041fcb4949c752cfb1e7fdbe
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-03-09 22:02:24 +0800

    Update q8_find_uppercase.py

--- p04/q8_find_uppercase.py
+++ p04/q8_find_uppercase.py
@@ -4,14 +4,12 @@ word = input('Enter word: ')
 # Funtion
 def find_num_uppercase(str):
     total = 0
-    i = 0
-    while i <len(str):
-        if 64 < ord(str[i]) < 91:
-            total += 1
-            i += 1
+    if len(str) > 0:
+        if 64 < ord(str[0]) < 91:
+            return 1 + find_num_uppercase(str[1:])
         else:
-            i += 1
-    print(total)
+            return find_num_uppercase(str[1:])
+    else: return 0
 
 # Main
-find_num_uppercase(word)
+print(find_num_uppercase(word))

99e1febb66d0927501270672f778503b5fa9041f
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-03-09 21:22:57 +0800

    Update q8_find_uppercase.py

--- p04/q8_find_uppercase.py
+++ p04/q8_find_uppercase.py
@@ -1,1 +1,17 @@
+# Enter Word
+word = input('Enter word: ')
 
+# Funtion
+def find_num_uppercase(str):
+    total = 0
+    i = 0
+    while i <len(str):
+        if 64 < ord(str[i]) < 91:
+            total += 1
+            i += 1
+        else:
+            i += 1
+    print(total)
+
+# Main
+find_num_uppercase(word)

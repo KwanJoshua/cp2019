6ab8dfa77d30539eb8a979492a9981c0f5ea4bf7
KwanJoshua <47781272+KwanJoshua@users.noreply.github.com>
2019-03-09 22:05:20 +0800

    Update q5_count_letter.py

--- p04/q5_count_letter.py
+++ p04/q5_count_letter.py
@@ -1,1 +1,16 @@
+# Enter Input
+word = input('Enter word: ')
+letter = input('Enter letter: ')
 
+# Funtion
+def count_letter(str, ch):
+    if len(str) > 0:
+        if str[0] == ch:
+            return 1 + count_letter(str[1:], ch)
+        else:
+            return count_letter(str[1:], ch)
+    else:
+        return 0
+
+# Main
+print(count_letter(word, letter))
